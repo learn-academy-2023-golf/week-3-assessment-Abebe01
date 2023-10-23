94a3a4d671a7b83f53c79bce5998abd85285ea6d
Asrat <aabebe.navy@gmail.com>
2023-10-23 08:52:37 -0700

    Abebe week 3 asses

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,6 +12,13 @@
 
 // --------------------1) Prompt: Create a function that takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function.
 
+const fibonacciSequence = num > 2
+let 
+if (num > 2) {
+ return 
+
+//  I think I'm having a hard time grasping how to do this. I looked uo the fibonacci sequence and understand the concept of it. But I dont get the idea of inputting one number and returning an array; let alone an array that includes the sequence. I'm sure it would need a conditional to decide what to do/ return. But im still not sure how you would return an entire array from one number. 
+
 // a) Create a test with expect statements for each of the variables provided.
 
 const fibonacciLength1 = 6
@@ -23,9 +30,19 @@ const fibonacciLength2 = 10
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+//Create a for loop/ conditional statement that takes in a number and returns an array. 
 
 // --------------------2) For the following prompt, use ChatGPT or an equivalent AI tool to help you solve the problem. The prompt will require the use of a JavaScript method called Object.values()
 // Prompt: Create a function that takes in an object and returns an array of the object's values sorted from least to greatest.
+
+function sortObjectValues(obj) {
+  const values = Object.values(obj);
+  values.sort((key, value) => key - value);
+  return values
+}
+
+module.exports = sortObjectValues;
+
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -38,6 +55,22 @@ const studyMinutesWeek1 = {
   friday: 15,
   saturday: 60
 }
+
+const sortObjectValues = require('./sortObjectValues');
+
+test('sortObjectValues for studyMinutesWeek1', () => {
+  expect(sortObjectValues({
+    sunday: 90,
+    monday: 30,
+    tuesday: 20,
+    wednesday: 15,
+    thursday: 30,
+    friday: 15,
+    saturday: 60
+  })).toEqual([15, 15, 20, 30, 30, 60, 90]);
+});
+
+
 // Expected output: [15, 15, 20, 30, 30, 60, 90]
 
 const studyMinutesWeek2 = {
@@ -49,8 +82,31 @@ const studyMinutesWeek2 = {
   friday: 15,
   saturday: 65
 }
+
+test('sortObjectValues for studyMinutesWeek2', () => {
+  expect(sortObjectValues({
+    sunday: 100,
+    monday: 10,
+    tuesday: 45,
+    wednesday: 60,
+    thursday: 20,
+    friday: 15,
+    saturday: 65
+  })).toEqual([10, 15, 20, 45, 60, 65, 100]);
+});
+
 // Expected output: [10, 15, 20, 45, 60, 65, 100]
 
 // b) Create the function that makes the test pass.
 
+function sortObjectValues(obj) {
+  const values = Object.values(obj);
+  values.sort((key, value) => key - value);
+  return values
+}
+
+
 // Pseudo code:
+// Create function to sort object values 
+// Create test for both variables 
+// test each 
--- interview-questions.md
+++ interview-questions.md
@@ -8,30 +8,34 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is a javascript extension for react. And one syntax differnce between HTML and JSX is class and className. 
 
-Researched answer:
+Researched answer: JSX (JavaScript XML) is a syntax extension for JavaScript, primarily used in React to define the structure and appearance of user interface components. One syntax difference between HTML and JSX is that you use className instead of class in JSX for defining CSS classes. This is because class is a reserved keyword in JavaScript, and JSX needs to be transformed into valid JavaScript.
 
 2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a package manager you can add or run on your local device. 
 
-Researched answer:
+Researched answer: Yarn is indeed a package manager, used to manage dependencies in a project. When you run the yarn command in the terminal, it fetches and installs packages defined in the package.json file.
 
 3. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State values and props are both used in React, but state values are used to store or "state" data in one file or component. While props can pass data on to other components. 
 
-Researched answer:
+Researched answer: 
+
+State values are used to store and manage data within a component. They are mutable and can be changed within the component. State is typically used for data that can change over time, and when state is updated, it triggers a re-render of the component to reflect the new data.
+
+Props (short for properties) are used to pass data from a parent component to a child component. Props are read-only and cannot be modified by the child component that receives them. They are used to pass data from higher-level components down the component tree, making it easy to share data between components.
 
 4. STRETCH: Which is the difference between a div tag and a span tag?
 
-Researched answer:
+Researched answer: A div tag serves as an divider in the page, across the entire page. While a span tag only spans the element or word specified; not the entire page. 
 
 ## Looking Ahead: Terms for Next Week
 
 1. DOM: 
 
-2. Object-oriented programming:
+2. Object-oriented programming: A programming method used to manage large operating/software systems. 
 
-3. Ruby:
+3. Ruby: an open-source object-oriented language.
